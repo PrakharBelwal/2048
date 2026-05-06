100b4d3efd0fe87a3618dcefd41b7635409db9a0
PrakharBelwal <prakhar.belwal@gmail.com>
2026-05-07 05:24:46 +0530

    Init and Board designed

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,60 @@
+function GameBoard(){
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center outline-0" tabIndex="0" onKeyDown={(e)=>{console.log(e)}}>
+        <div className="bg-[#BBADA0] w-[70vmin] h-[70vmin] rounded-xl grid grid-cols-4 grid-rows-4 gap-2 p-2">
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+          <div className="bg-[#efe6db] rounded">
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GameBoard
