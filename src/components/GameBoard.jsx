fb10c6e480364b943f33d521bb83f36a4b6a8fe4
PrakharBelwal <prakhar.belwal@gmail.com>
2026-05-09 03:27:09 +0530

    spawn logic done

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,56 +1,38 @@
+import { useState } from "react"
+import spawn from "../utility/spawn"
+import { useEffect } from "react"
+
 function GameBoard(){
+
+  const [board, setBoard] = useState([
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+  ])
+
+  function handleEvent(){
+    setBoard(prev => spawn(prev))
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown",handleEvent)
+    return () => {
+      window.removeEventListener("keydown", handleEvent)
+    }
+  },[])
+
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center outline-0" tabIndex="0" onKeyDown={(e)=>{console.log(e)}}>
+      <div className="w-full h-screen flex items-center justify-center outline-0">
         <div className="bg-[#BBADA0] w-[70vmin] h-[70vmin] rounded-xl grid grid-cols-4 grid-rows-4 gap-2 p-2">
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
-          <div className="bg-[#efe6db] rounded">
-
-          </div>
+        {board.map((e)=>e.map((e,idx)=>{
+          return(
+            <div key={idx} className="bg-[#efe6db] rounded flex items-center justify-center text-2xl">
+              {e === 0 ? "" : e}
+            </div>
+          )
+        }))}
         </div>
       </div>
     </>
---
+++ src/utility/spawn.js
@@ -0,0 +1,31 @@
+function randomCoord(board){
+  let emptyCell = []
+  board.forEach((arr,x) =>{
+    arr.forEach((elem,y) =>{
+      if(elem === 0){
+        emptyCell = [...emptyCell, [x,y]]
+      }
+    })
+  })
+  if(emptyCell.length === 0){
+    return [-1,-1]
+  }
+  const coords = Math.floor(Math.random()*emptyCell.length)
+  return emptyCell[coords]
+}
+
+export default function spawn(board){
+  let newBoard = board.map(row => [...row])
+  const probOfSpawn4 = Math.floor(Math.random()*2)
+  let [x,y] = randomCoord(board)
+  if(x != -1 && y != -1){
+    if(probOfSpawn4){
+      newBoard[x][y] = 4
+    }else{
+      newBoard[x][y] = 2
+    }
+  }else{
+    console.log("Game Over")
+  }
+  return newBoard
+}
